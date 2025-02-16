1e48ee90d3782afe2d6237e7daacd96941e6d7cb
Danial <dmunatayev.dev@gmail.com>
2025-02-16 16:17:22 +0100

    Fixed SettingsSetter in domain package

--- src/domain/SettingsSetter.java
+++ src/domain/SettingsSetter.java
@@ -4,13 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * The SettingsSetter class is responsible for setting the font and visibility parameters
+ * to the components of a given JFrame window. It also allows certain components to be
+ * ignored from these settings.
+ */
 public class SettingsSetter {
 
     private static Font font;
-    private static ArrayList<Component> ignoredComponents = new ArrayList<>();
+    private static final ArrayList<Component> ignoredComponents = new ArrayList<>();
 
+    /**
+     * Constructs a SettingsSetter object and sets the icon image and font for the given window.
+     *
+     * @param window the JFrame window to which the settings will be applied
+     */
+    @SuppressWarnings("CallToPrintStackTrace")
     public SettingsSetter(JFrame window) {
-
         try {
             window.setIconImage(new ImageIcon("src/Resources/images/seaBattleIcon.png").getImage());
             font = Font.createFont(Font.TRUETYPE_FONT,
@@ -22,6 +32,11 @@ public class SettingsSetter {
         setParametersToObjects(window);
     }
 
+    /**
+     * Sets the font and visibility parameters to the given component and its children.
+     *
+     * @param component the component to which the settings will be applied
+     */
     public static void setParametersToObjects(Component component) {
         if (!ignoredComponents.contains(component)) {
             if (!(component.getFont() == null)) {
@@ -34,6 +49,11 @@ public class SettingsSetter {
         }
     }
 
+    /**
+     * Adds a component to the list of components that will be ignored from the settings.
+     *
+     * @param component the component to be ignored
+     */
     public static void ignoreSettingParametersToObjects(Component component) {
         ignoredComponents.add(component);
     }
